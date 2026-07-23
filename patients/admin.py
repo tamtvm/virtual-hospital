6ba842cd057e682b3ff506a86d309f111ac9f21d
t am <tamtvm@hotmail.com>
2026-07-23 13:18:02 -0400

    feat: register patient model in admin panel

--- patients/admin.py
+++ patients/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Patient
 
-# Register your models here.
+@admin.register(Patient)
+class PatientAdmin(admin.ModelAdmin):
+    # Admin list view
+    list_display = ('name', 'species', 'location', 'id_number', 'created_at')
+    
+    # Search bar
+    search_fields = ('name', 'id_number')
+    
+    # Filters
+    list_filter = ('species', 'location', 'sex')
