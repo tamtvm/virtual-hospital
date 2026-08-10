cd64db0d7d399e277aa2cdcea84a57cec1330725
t am <tamtvm@hotmail.com>
2026-08-10 17:09:45 -0400

    point API base url to render

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -1,4 +1,4 @@
 // --- Application configuration (environment dependent values) ---
-export const API_BASE_URL = 'http://127.0.0.1:8000/api';
+export const API_BASE_URL = 'https://virtual-hospital-b471.onrender.com/api';
 export const AVATAR_BASE_PATH = 'assets/avatars';
 export const DEFAULT_AVATAR = 'assets/default-avatar.png';
