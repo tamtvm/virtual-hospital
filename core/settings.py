a57dafba871c00e10e029e1cdf1fd6c26f042911
t am <tamtvm@hotmail.com>
2026-08-13 07:29:29 -0400

    chore: local dev origins for next.js

--- core/settings.py
+++ core/settings.py
@@ -147,6 +147,10 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:8000",
     "http://127.0.0.1:5500",
     "http://localhost:5500",
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+    "http://localhost:8788",
+    "http://127.0.0.1:8788",
     "https://virtual-hospital.pages.dev",
 ]
 
